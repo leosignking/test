d442324d4f9439de8bab9d4a230c10c908c57a9c
Sridhar Reddy <redreddy.sridhar@gmail.com>
2020-02-15 20:42:56 -0500

    First Commit

---
+++ src/main/java/com/learn/test/Test.java
@@ -0,0 +1,11 @@
+package com.learn.test;
+
+/**
+ * @author leosignking
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Maint method exectued");
+    }
+}
